9694eabdad5200b964e1ab7cd4a2003faffcd235
Podryez Nikita <nikitapodryez@gmail.com>
2026-06-12 23:23:12 +0300

    Hardcode Azure API URL

--- src/config/api.js
+++ src/config/api.js
@@ -2,6 +2,9 @@
 
 console.log("VITE_API_URL =", import.meta.env.VITE_API_URL);
 
+//export const API_BASE =
+//    import.meta.env.VITE_API_URL ||
+//    "https://localhost:7267";
+
 export const API_BASE =
-    import.meta.env.VITE_API_URL ||
-    "https://localhost:7267";
+    "https://cartradingshopapi20260611195416-bnhwgrh7c3dvhpc6.swedencentral-01.azurewebsites.net";
