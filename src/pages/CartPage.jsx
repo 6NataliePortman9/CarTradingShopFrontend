1ac2b8205c8b931a0c65d6c015a7106ffc1ef07a
Podryez Nikita <nikitapodryez@gmail.com>
2026-06-17 01:45:10 +0300

    Update button text to include 'Sold' emoji

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -98,7 +98,7 @@ export default function CartPage() {
                     }}
                     disabled
                 >
-                    🚫 Sold
+                    🚫
                 </Button>
             );
         }
